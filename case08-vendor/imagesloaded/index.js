efe8f3c37ad0e3b55d3fdcf72fe63ba99d4ba294
honger <honger05@126.com>
2015-10-23 16:05:13 +0800

    f

---
+++ case08-vendor/imagesloaded/index.js
@@ -0,0 +1,12 @@
+var gridEl = document.getElementById('grid');
+
+/////////////////
+// 图片加载完了才执行回调 //
+/////////////////
+imagesLoaded(gridEl, function() {
+	// 再执行 砌墙式布局;
+	new Masonry(gridEl, {
+		itemSelector: 'li'
+		// transitionDuration : 0
+	});
+})
--- case08-vendor/masonry/index.js
+++ case08-vendor/masonry/index.js
@@ -1,5 +1,5 @@
 
 $('.grid').masonry({
-	itemSelector: '.grid-item',
-	columnWidth: 200
+	itemSelector: '.grid-item'
+	// columnWidth: 200
 });
